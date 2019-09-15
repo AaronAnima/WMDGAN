9e15cd21dc137778f5127114fcd687f938ee5124
AaronAnima <wmingd@pku.edu.cn>
2019-09-15 12:40:37 +0800

    efef

---
+++ models.py
@@ -0,0 +1,190 @@
+import tensorflow as tf
+import tensorlayer as tl
+import numpy as np
+from tensorlayer.layers import Input, Dense, DeConv2d, Reshape, BatchNorm2d, Conv2d, Flatten, BatchNorm, Concat, GaussianNoise
+from config import flags
+from utils import WeightNorm
+
+
+def get_G(shape_z, ngf=64):    # Dimension of gen filters in first conv layer. [64]
+    # w_init = tf.glorot_normal_initializer()
+    w_init = tf.random_normal_initializer(stddev=0.02)
+    gamma_init = tf.random_normal_initializer(1., 0.02)
+    n_extra_layers = 0
+    isize = 64
+    cngf, tisize = ngf // 2, 4
+    while tisize != isize:
+        cngf = cngf * 2
+        tisize = tisize * 2
+    ni = Input(shape_z)
+    nn = Reshape(shape=[-1, 1, 1, 128])(ni)
+    nn = DeConv2d(cngf, (4, 4), (1, 1), W_init=w_init, b_init=None, padding='VALID')(nn)
+    nn = BatchNorm(decay=0.9, act=tf.nn.relu, gamma_init=gamma_init, name=None)(nn)
+    print(nn.shape)
+    csize, cndf = 4, cngf
+    while csize < isize // 2:
+        nn = DeConv2d(cngf // 2, (4, 4), (2, 2), W_init=w_init, b_init=None)(nn)
+        nn = BatchNorm(decay=0.9, act=tf.nn.relu, gamma_init=gamma_init, name=None)(nn)
+        print(nn.shape)
+        cngf = cngf // 2
+        csize = csize * 2
+
+    for t in range(n_extra_layers):
+        nn = DeConv2d(cngf, (3, 3), (1, 1), W_init=w_init, b_init=None)(nn)
+        nn = BatchNorm(decay=0.9, act=tf.nn.relu, gamma_init=gamma_init, name=None)(nn)
+        print(nn.shape)
+
+    nn = DeConv2d(3, (4, 4), (2, 2), act=tf.nn.tanh, W_init=w_init, b_init=None)(nn)
+    print(nn.shape)
+
+    return tl.models.Model(inputs=ni, outputs=nn)
+
+
+# E is reverse of G without activation in the output layer
+def get_E(shape):
+    w_init = tf.random_normal_initializer(stddev=0.02)
+    gamma_init = tf.random_normal_initializer(1., 0.02)
+    lrelu = lambda x: tf.nn.leaky_relu(x, 0.2)
+    ngf = 64
+
+    ni = Input(shape)
+    nn = Conv2d(ngf, (4, 4), (2, 2), act=None, W_init=w_init, b_init=None)(ni)
+    nn = BatchNorm2d(decay=0.9, act=lrelu, gamma_init=gamma_init)(nn)
+
+    for i in range(3):
+        ngf = ngf * 2
+        nn = Conv2d(ngf, (4, 4), (2, 2), act=None, W_init=w_init, b_init=None)(nn)
+        nn = BatchNorm2d(decay=0.9, act=lrelu, gamma_init=gamma_init)(nn)
+
+    nn = Conv2d(flags.z_dim, (4, 4), (1, 1), act=None, W_init=w_init, b_init=None, padding='VALID')(nn)
+    nz = Reshape(shape=[-1, 128])(nn)
+
+    return tl.models.Model(inputs=ni, outputs=nz)
+
+def get_img_D(shape):
+    w_init = tf.random_normal_initializer(stddev=0.02)
+    gamma_init = tf.random_normal_initializer(1., 0.02)
+    lrelu = lambda x: tf.nn.leaky_relu(x, 0.2)
+    ndf = 64
+    isize = 64
+    n_extra_layers = 0
+
+    ni = Input(shape)
+    n = Conv2d(ndf, (4, 4), (2, 2), act=None, W_init=w_init, b_init=None)(ni)
+    csize, cndf = isize / 2, ndf
+
+    for t in range(n_extra_layers):
+        n = Conv2d(cndf, (3, 3), (1, 1), act=None, W_init=w_init, b_init=None)(n)
+        n = BatchNorm2d(decay=0.9, act=lrelu, gamma_init=gamma_init)(n)
+
+    while csize > 4:
+        out_feat = cndf * 2
+        n = Conv2d(out_feat, (4, 4), (2, 2), act=None, W_init=w_init, b_init=None)(n)
+        n = BatchNorm2d(decay=0.9, act=lrelu, gamma_init=gamma_init)(n)
+        cndf = cndf * 2
+        csize = csize / 2
+
+    n = Conv2d(1, (4, 4), (1, 1), act=None, W_init=w_init, b_init=None, padding='VALID')(n)
+
+    return tl.models.Model(inputs=ni, outputs=n)
+
+
+
+def get_z_D(shape_z):
+    w_init = tf.random_normal_initializer(stddev=0.02)
+    lrelu = lambda x: tf.nn.leaky_relu(x, 0.2)
+    nz = Input(shape_z)
+    n = Dense(n_units=750, act=lrelu, W_init=w_init)(nz)
+    n = Dense(n_units=750, act=lrelu, W_init=w_init)(n)
+    n = Dense(n_units=750, act=lrelu, W_init=w_init)(n)
+    n = Dense(n_units=1, act=None, W_init=w_init, b_init=None)(n)
+    return tl.models.Model(inputs=nz, outputs=n)
+
+
+def get_trans_func(shape=[None, flags.z_dim]):
+    w_init = tf.random_normal_initializer(stddev=0.02)
+    act = 'relu'  # lambda x : tf.nn.leaky_relu(x, 0.2)
+
+    ni = Input(shape)
+    nn = Dense(n_units=flags.z_dim, act=act, W_init=w_init)(ni)
+    nn = Dense(n_units=flags.z_dim, act=act, W_init=w_init)(nn)
+    nn = Dense(n_units=flags.z_dim, act=act, W_init=w_init)(nn)
+    nn = Dense(n_units=flags.z_dim, W_init=w_init)(nn)
+
+    return tl.models.Model(inputs=ni, outputs=nn)
+
+
+# def get_img_D(shape):
+#     df_dim = 8
+#     w_init = tf.random_normal_initializer(stddev=0.02)
+#     gamma_init = tf.random_normal_initializer(1., 0.02)
+#     lrelu = lambda x: tf.nn.leaky_relu(x, 0.2)
+#     ni = Input(shape)
+#     n = Conv2d(df_dim, (5, 5), (2, 2), act=None, W_init=w_init, b_init=None)(ni)
+#     n = BatchNorm2d(decay=0.9, act=lrelu, gamma_init=gamma_init)(n)
+#     n = Conv2d(df_dim * 2, (5, 5), (1, 1), act=None, W_init=w_init, b_init=None)(n)
+#     n = BatchNorm2d(decay=0.9, act=lrelu, gamma_init=gamma_init)(n)
+#     n = Conv2d(df_dim * 4, (5, 5), (2, 2), act=None, W_init=w_init, b_init=None)(n)
+#     n = BatchNorm2d(decay=0.9, act=lrelu, gamma_init=gamma_init)(n)
+#     n = Conv2d(df_dim * 8, (5, 5), (1, 1), act=None, W_init=w_init, b_init=None)(n)
+#     n = BatchNorm2d(decay=0.9, act=lrelu, gamma_init=gamma_init)(n)
+#     n = Conv2d(df_dim * 8, (5, 5), (2, 2), act=None, W_init=w_init, b_init=None)(n)
+#     n = BatchNorm2d(decay=0.9, act=lrelu, gamma_init=gamma_init)(n)
+#     nf = Flatten(name='flatten')(n)
+#     n = Dense(n_units=1, act=None, W_init=w_init)(nf)
+#     return tl.models.Model(inputs=ni, outputs=n, name='img_Discriminator')
+
+
+
+#
+# def get_classifier(shape=[None, flags.z_dim], df_dim=64):
+#     w_init = tf.random_normal_initializer(stddev=0.02)
+#     act = 'relu'  # lambda x : tf.nn.leaky_relu(x, 0.2)
+#
+#     ni = Input(shape)
+#     nn = Dense(n_units=flags.z_dim, act=act, W_init=w_init)(ni)
+#     nn = Dense(n_units=flags.z_dim, act=act, W_init=w_init)(nn)
+#     nn = Dense(n_units=flags.z_dim, act=act, W_init=w_init)(nn)
+#     nn = Dense(n_units=1, W_init=w_init)(nn)
+#
+#     return tl.models.Model(inputs=ni, outputs=nn)
+
+
+# def get_G(shape_z, gf_dim=64):    # Dimension of gen filters in first conv layer. [64]
+#
+#     image_size = 32
+#     s16 = image_size // 16
+#     # w_init = tf.glorot_normal_initializer()
+#     w_init = tf.random_normal_initializer(stddev=0.02)
+#     gamma_init = tf.random_normal_initializer(1., 0.02)
+#
+#     ni = Input(shape_z)
+#     nn = Dense(n_units=(gf_dim * 16 * s16 * s16), W_init=w_init, b_init=None)(ni)
+#     nn = Reshape(shape=[-1, s16, s16, gf_dim * 16])(nn) # [-1, 2, 2, gf_dim * 8]
+#     nn = BatchNorm(decay=0.9, act=tf.nn.relu, gamma_init=gamma_init, name=None)(nn)
+#     nn = DeConv2d(gf_dim * 8, (5, 5), (2, 2), W_init=w_init, b_init=None)(nn) # [-1, 4, 4, gf_dim * 4]
+#     nn = BatchNorm2d(decay=0.9, act=tf.nn.relu, gamma_init=gamma_init)(nn)
+#     nn = DeConv2d(gf_dim * 4, (5, 5), (2, 2), W_init=w_init, b_init=None)(nn) # [-1, 8, 8, gf_dim * 2]
+#     nn = BatchNorm2d(decay=0.9, act=tf.nn.relu, gamma_init=gamma_init)(nn)
+#     nn = DeConv2d(gf_dim * 2, (5, 5), (2, 2), W_init=w_init, b_init=None)(nn) # [-1, 16, 16, gf_dim *]
+#     nn = BatchNorm2d(decay=0.9, act=tf.nn.relu, gamma_init=gamma_init)(nn)
+#     nn = DeConv2d(gf_dim, (5, 5), (2, 2), b_init=None, W_init=w_init)(nn) # [-1, 32, 32, 3]
+#     nn = BatchNorm2d(decay=0.9, act=tf.nn.relu, gamma_init=gamma_init)(nn)
+#     nn = DeConv2d(3, (5, 5), (2, 2), act=tf.nn.tanh, W_init=w_init)(nn)  # [-1, 64, 64, 3]
+#
+#     return tl.models.Model(inputs=ni, outputs=nn, name='generator')
+
+# def get_E(shape):
+#     w_init = tf.random_normal_initializer(stddev=0.02)
+#     gamma_init = tf.random_normal_initializer(1., 0.02)
+#     lrelu = lambda x: tf.nn.leaky_relu(x, 0.2)
+#     ni = Input(shape)   # (1, 64, 64, 3)
+#     n = Conv2d(3, (5, 5), (2, 2), act=None, W_init=w_init, b_init=None)(ni)  # (1, 16, 16, 3)
+#     n = BatchNorm2d(decay=0.9, act=lrelu, gamma_init=gamma_init)(n)
+#     n = Conv2d(32, (5, 5), (1, 1), padding="VALID", act=None, W_init=w_init, b_init=None)(n)  # (1, 12, 12, 32)
+#     n = BatchNorm2d(decay=0.9, act=lrelu, gamma_init=gamma_init)(n)
+#     n = Conv2d(64, (5, 5), (2, 2), act=None, W_init=w_init, b_init=None)(n)  # (1, 6, 6, 64)
+#     n = BatchNorm2d(decay=0.9, act=lrelu, gamma_init=gamma_init)(n)
+#     n = Flatten(name='flatten')(n)
+#     nz = Dense(n_units=flags.z_dim, act=None, W_init=w_init)(n)
+#     return tl.models.Model(inputs=ni, outputs=nz, name='encoder')
---
+++ train.py
@@ -0,0 +1,324 @@
+import os, time, multiprocessing
+import numpy as np
+import tensorflow as tf
+import tensorlayer as tl
+from config import flags
+from data import get_dataset_train
+from models import get_G, get_E, get_z_D, get_trans_func, get_img_D
+import random
+import argparse
+import math
+import scipy.stats as stats
+import tensorflow_probability as tfp
+
+import ipdb
+
+
+G = get_G([None, flags.z_dim])
+D = get_img_D([None, flags.img_size_h, flags.img_size_w, flags.c_dim])
+E = get_E([None, flags.img_size_h, flags.img_size_w, flags.c_dim])
+D_z = get_z_D([None, flags.z_dim])
+C = get_z_D([None, flags.z_dim])
+f_ab = get_trans_func([None, flags.z_dim])
+f_ba = get_trans_func([None, flags.z_dim])
+D_zA = get_z_D([None, flags.z_dim])
+D_zB = get_z_D([None, flags.z_dim])
+
+
+def KStest(real_z, fake_z):
+    p_list = []
+    for i in range(flags.batch_size_train):
+        _, tmp_p = stats.ks_2samp(fake_z[i], real_z[i])
+        p_list.append(tmp_p)
+    return np.min(p_list), np.mean(p_list)
+
+
+def train_GE(con=False):
+    dataset, len_dataset = get_dataset_train()
+    len_dataset = flags.len_dataset
+    print(con)
+    if con:
+        G.load_weights('./checkpoint/G.npz')
+        D.load_weights('./checkpoint/D.npz')
+        E.load_weights('./checkpoint/E.npz')
+        D_z.load_weights('./checkpoint/Dz.npz')
+
+    G.train()
+    D.train()
+    E.train()
+    D_z.train()
+
+    n_step_epoch = int(len_dataset // flags.batch_size_train)
+    n_epoch = int(flags.step_num // n_step_epoch)
+
+    lr_G = flags.lr_G
+    lr_E = flags.lr_E
+    lr_D = flags.lr_D
+    lr_Dz = flags.lr_Dz
+
+    d_optimizer = tf.optimizers.Adam(lr_D, beta_1=flags.beta1, beta_2=flags.beta2)
+    g_optimizer = tf.optimizers.Adam(lr_G, beta_1=flags.beta1, beta_2=flags.beta2)
+    e_optimizer = tf.optimizers.Adam(lr_E, beta_1=flags.beta1, beta_2=flags.beta2)
+    dz_optimizer = tf.optimizers.Adam(lr_Dz, beta_1=flags.beta1, beta_2=flags.beta2)
+    # tfd = tfp.distributions
+    # dist_normal = tfd.Normal(loc=0., scale=1.)
+    # dist_Bernoulli = tfd.Bernoulli(probs=0.5)
+    # dist_beta = tfd.Beta(0.5, 0.5)
+    eval_batch = None
+    for step, image_labels in enumerate(dataset):
+        '''
+        log = " ** new learning rate: %f (for GAN)" % (lr_v.tolist()[0])
+        print(log)
+        '''
+        if step == 0:
+            eval_batch  = image_labels[0]
+        batch_imgs = image_labels[0]
+
+        # ipdb.set_trace()
+
+        epoch_num = step // n_step_epoch
+        with tf.GradientTape(persistent=True) as tape:
+            z = np.random.normal(loc=0.0, scale=1, size=[flags.batch_size_train, flags.z_dim]).astype(np.float32)
+            tl.visualize.save_images(batch_imgs.numpy(), [8, 8],
+                                     '{}/raw_samples.png'.format(flags.sample_dir))
+            fake_z = E(batch_imgs)
+            fake_imgs = G(fake_z)
+            fake_logits = D(fake_imgs)
+            real_logits = D(batch_imgs)
+            fake_logits_z = D(G(z))
+            real_z_logits = D_z(z)
+            fake_z_logits = D_z(fake_z)
+
+            e_loss_z = - tl.cost.sigmoid_cross_entropy(fake_z_logits, tf.zeros_like(fake_z_logits)) + \
+                       tl.cost.sigmoid_cross_entropy(fake_z_logits, tf.ones_like(fake_z_logits))
+
+            recon_loss = flags.lamba_recon * tl.cost.absolute_difference_error(batch_imgs, fake_imgs, is_mean=True)
+            g_loss_x = - tl.cost.sigmoid_cross_entropy(fake_logits, tf.zeros_like(fake_logits)) + \
+                       tl.cost.sigmoid_cross_entropy(fake_logits, tf.ones_like(fake_logits))
+            g_loss_z = - tl.cost.sigmoid_cross_entropy(fake_logits_z, tf.zeros_like(fake_logits_z)) + \
+                       tl.cost.sigmoid_cross_entropy(fake_logits_z, tf.ones_like(fake_logits_z))
+
+            e_loss = recon_loss + e_loss_z
+            g_loss = recon_loss + g_loss_x + g_loss_z
+
+            d_loss = tl.cost.sigmoid_cross_entropy(real_logits, tf.ones_like(real_logits)) + \
+                     tl.cost.sigmoid_cross_entropy(fake_logits, tf.zeros_like(fake_logits)) + \
+                     tl.cost.sigmoid_cross_entropy(fake_logits_z, tf.zeros_like(fake_logits_z))
+
+            dz_loss = tl.cost.sigmoid_cross_entropy(fake_z_logits, tf.zeros_like(fake_z_logits)) + \
+                      tl.cost.sigmoid_cross_entropy(real_z_logits, tf.ones_like(real_z_logits))
+
+        # Updating Encoder
+        grad = tape.gradient(e_loss, E.trainable_weights)
+        e_optimizer.apply_gradients(zip(grad, E.trainable_weights))
+
+        # Updating Generator
+        grad = tape.gradient(g_loss, G.trainable_weights)
+        g_optimizer.apply_gradients(zip(grad, G.trainable_weights))
+
+        # Updating Discriminator
+        grad = tape.gradient(d_loss, D.trainable_weights)
+        d_optimizer.apply_gradients(zip(grad, D.trainable_weights))
+
+        # Updating D_z & D_h
+        grad = tape.gradient(dz_loss, D_z.trainable_weights)
+        dz_optimizer.apply_gradients(zip(grad, D_z.trainable_weights))
+
+        # basic
+        if np.mod(step, flags.show_freq) == 0 and step != 0:
+            print("Epoch: [{}/{}] [{}/{}] e_loss: {:.5f}, g_loss: {:.5f}, d_loss: {:.5f}, "
+                  "dz_loss: {:.5f}, recon: {:.5f}".format
+                  (epoch_num, n_epoch, step, n_step_epoch, e_loss, g_loss,
+                   d_loss, dz_loss, recon_loss))
+            # Kstest
+            p_min, p_avg = KStest(z, fake_z)
+            print("kstest: min:{}, avg:{}", p_min, p_avg)
+
+        if np.mod(step, n_step_epoch) == 0 and step != 0:
+            G.save_weights('{}/G.npz'.format(flags.checkpoint_dir), format='npz')
+            D.save_weights('{}/D.npz'.format(flags.checkpoint_dir), format='npz')
+            E.save_weights('{}/E.npz'.format(flags.checkpoint_dir), format='npz')
+            D_z.save_weights('{}/Dz.npz'.format(flags.checkpoint_dir), format='npz')
+            # G.train()
+        if np.mod(step, flags.eval_step) == 0 and step != 0:
+            z = np.random.normal(loc=0.0, scale=1, size=[flags.batch_size_train, flags.z_dim]).astype(np.float32)
+            G.eval()
+            recon_imgs = G(E(eval_batch))
+            result = G(z)
+            G.train()
+            tl.visualize.save_images(result.numpy(), [8, 8],
+                                     '{}/sample_{:02d}_{:04d}.png'.format(flags.sample_dir,
+                                                                            step // n_step_epoch, step))
+            tl.visualize.save_images(recon_imgs.numpy(), [8, 8],
+                                     '{}/recon_{:02d}_{:04d}.png'.format(flags.sample_dir,
+                                                                            step // n_step_epoch, step))
+        del tape
+
+
+def train_C():
+    dataset, len_dataset = get_dataset_train()
+    len_dataset = flags.len_dataset
+    # G.load_weights('./checkpoint/{}/G.npz'.format(flags.param_dir))
+    # E.load_weights('./checkpoint/{}/E.npz'.format(flags.param_dir))
+    G.eval()
+    E.eval()
+    C.train()
+
+    c_optimizer = tf.optimizers.Adam(flags.lr_C, beta_1=flags.beta1, beta_2=flags.beta2)
+
+    n_step_epoch = int(len_dataset // flags.batch_size_train)
+    n_epoch = int(flags.step_num // n_step_epoch)
+    acc_sum = 0
+    acc_step = 0
+    for step, image_labels in enumerate(dataset):
+        '''
+        log = " ** new learning rate: %f (for GAN)" % (lr_v.tolist()[0])
+        print(log)
+        '''
+        batch_imgs = image_labels[0]
+        batch_labels = image_labels[1]
+        batch_labels = (batch_labels + 1) / 2
+        batch_labels = tf.reshape(batch_labels, [flags.batch_size_train, 1])
+        batch_labels = tf.cast(batch_labels, tf.float32)
+        epoch_num = step // n_step_epoch
+
+        with tf.GradientTape(persistent=True) as tape:
+            real_z = E(batch_imgs)
+            z_logits = C(real_z)
+            z_logits = tf.cast(z_logits, tf.float32)
+            loss_c = tl.cost.sigmoid_cross_entropy(z_logits, batch_labels)
+            logits = ((tf.sign(z_logits * 2 - 1) + 1)/2)
+            labels = batch_labels
+            acc_num = flags.batch_size_train + tf.reduce_sum(logits * labels) - tf.reduce_sum(logits) \
+                      - tf.reduce_sum(labels)
+            batch_acc = acc_num / flags.batch_size_train
+            acc_sum += batch_acc
+            acc_step += 1
+        # Updating Encoder
+        grad = tape.gradient(loss_c, C.trainable_weights)
+        c_optimizer.apply_gradients(zip(grad, C.trainable_weights))
+        del tape
+
+        # basic
+        if np.mod(step, flags.show_freq) == 0 and step != 0:
+            print("Epoch: [{}/{}] [{}/{}] batch_acc is {}, loss_c: {:.5f}".format
+                  (epoch_num, n_epoch, step, n_step_epoch, batch_acc, loss_c))
+
+        if np.mod(step, n_step_epoch) == 0 and step != 0:
+            C.save_weights('{}/G.npz'.format(flags.checkpoint_dir), format='npz')
+
+        if np.mod(step, flags.acc_step) == 0 and step != 0:
+            acc = acc_sum / acc_step
+            print("The avg step_acc is {:.3f} in {} step".format(acc, acc_step))
+            acc_sum = 0
+            acc_step = 0
+
+
+def train_F_D():
+    dataset, len_dataset = get_dataset_train()
+    len_dataset = flags.len_dataset
+
+    G.load_weights('./checkpoint/G.npz')
+    E.load_weights('./checkpoint/E.npz')
+    f_ab.train()
+    f_ba.train()
+    D_zA.train()
+    D_zB.train()
+    G.eval()
+    E.eval()
+
+    f_optimizer = tf.optimizers.Adam(flags.lr_F, beta_1=flags.beta1, beta_2=flags.beta2)
+    d_optimizer = tf.optimizers.Adam(flags.lr_D, beta_1=flags.beta1, beta_2=flags.beta2)
+
+    n_step_epoch = int(len_dataset // flags.batch_size_train)
+    n_epoch = int(flags.step_num // n_step_epoch)
+
+    for step, batch_imgs in enumerate(dataset):
+        '''
+        log = " ** new learning rate: %f (for GAN)" % (lr_v.tolist()[0])
+        print(log)
+        '''
+        image_a = batch_imgs[0]
+        image_b = batch_imgs[1]
+        epoch_num = step // n_step_epoch
+
+        with tf.GradientTape(persistent=True) as tape:
+            za_real = E()
+        # Updating Encoder
+        grad = tape.gradient(loss_c, C.trainable_weights)
+        c_optimizer.apply_gradients(zip(grad, C.trainable_weights))
+        del tape
+
+        # basic
+        if np.mod(step, flags.show_freq) == 0 and step != 0:
+            print("Epoch: [{}/{}] [{}/{}] batch_acc is {}, loss_c: {:.5f}".format
+                  (epoch_num, n_epoch, step, n_step_epoch, batch_acc, loss_c))
+
+        if np.mod(step, n_step_epoch) == 0 and step != 0:
+            C.save_weights('{}/G.npz'.format(flags.checkpoint_dir), format='npz')
+
+        if np.mod(step, flags.acc_step) == 0 and step != 0:
+            acc = acc_sum / acc_step
+            print("The avg step_acc is {:.3f} in {} step".format(acc, acc_step))
+            acc_sum = 0
+            acc_step = 0
+
+
+#### Main part, opt z to translate raw img to another latent manifold ####
+def opt_z():
+    dataset, len_dataset = get_dataset_train()
+    len_dataset = flags.len_dataset
+
+    G.eval()
+    E.eval()
+    C.eval()
+
+    z_optimizer = tf.optimizers.Adam(lr_C, beta_1=flags.beta1, beta_2=flags.beta2)
+
+
+    for step, image_labels in enumerate(dataset):
+        '''
+        log = " ** new learning rate: %f (for GAN)" % (lr_v.tolist()[0])
+        print(log)
+        '''
+        if step >= flags.sample_cnt:
+            break
+        print('Now start {} img'.format(str(step)))
+        batch_imgs = image_labels[0]
+
+        batch_labels = image_labels[1]
+        batch_labels = (batch_labels + 1) / 2
+        tran_label = tf.ones_like(batch_labels) - batch_labels
+
+        for step_z in range(flags.step_z):
+            with tf.GradientTape(persistent=True) as tape:
+
+                real_z = E(batch_imgs)
+                z = tf.Variable(real_z)
+                z_logits = C(z)
+                loss_z = tl.cost.sigmoid_cross_entropy(z_logits, tran_label)
+
+            # Updating Encoder
+            grad = tape.gradient(loss_z, z.trainable_weights)
+            z_optimizer.apply_gradients(zip(grad, z.trainable_weights))
+            opt_img = G(z)
+            del tape
+            if np.mod(step_z, 10) == 0:
+                tl.visualize.save_images(opt_img.numpy(), [8, 8],
+                                         '{}/opt_img{:02d}_step{:02d}.png'.format(flags.opt_sample_dir, step, step_z))
+
+
+        tl.visualize.save_images(batch_imgs.numpy(), [8, 8],
+                                 '{}/raw_img{:02d}.png'.format(flags.opt_sample_dir, step))
+        tl.visualize.save_images(opt_img.numpy(), [8, 8],
+                                 '{}/opt_img{:02d}.png'.format(flags.opt_sample_dir, step))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--mode', type=str, default='DWGAN', help='train or eval')
+    parser.add_argument('--is_continue', type=bool, default=False, help='load weights from checkpoints?')
+    args = parser.parse_args()
+    train_GE(con=args.is_continue)
+    train_C()
+    opt_z()
